0891e77dc030a23dbdbee8b4c85ffe28bdb37c0b
soccerJoshNumberNine_terminal <32210099+joshuaSmith2021@users.noreply.github.com>
2018-02-22 18:06:33 -0800

    put roster creator on homepage

--- script/homepage.js
+++ script/homepage.js
@@ -16,7 +16,23 @@ $(window).scroll(function () {
   }
   var desiredHeadlineOpacity = (75 - scroll) / 50;
   document.getElementById('headline').style.opacity = desiredHeadlineOpacity;
-  
+
   var desiredContentOpacity = scroll / 75;
   document.getElementById('content').style.opacity = desiredContentOpacity;
 });
+
+$('#addPlayer').click(function () {
+  $('#playerInputs').append('<br/><br/><input type="text" placeholder="Player Name" class="pName" onkeyup="updateRoster();"/>');
+});
+
+function updateRoster () {
+  var roster = [];
+  var names = document.getElementsByClassName('pName');
+  var i;
+
+  for (i = 0; i < names.length; i++) {
+    roster.push(names[i].value);
+  }
+
+  $('#rosterDisplay').html(JSON.stringify(roster));
+}
